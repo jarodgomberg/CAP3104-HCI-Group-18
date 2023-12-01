89699da7f3e0f762170ef2e04fde90ce2cdf1dac
jarod <70416222+jarodgomberg@users.noreply.github.com>
2023-11-30 21:07:37 -0500

    Edited leaderboard setup and display
    Added tabs for different difficulties on the leaderboard.

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,8 +1,10 @@
 //function to create and populate the leaderboard
 function createLeaderboard() {
-	const leaderboard = document.getElementById("score-list");
+	const easyLeaderboard = document.getElementById("score-list");
+	const mediumLeaderboard = document.getElementById("medium-list");
+	const hardLeaderboard = document.getElementById("hard-list");
 
-	if (!leaderboard) {
+	if (!easyLeaderboard) {
 		console.error("Leaderboard element not found.");
 		return;
 	}
@@ -13,26 +15,80 @@ function createLeaderboard() {
 	console.log("Fetched usernames from sessionStorage:", usernames);
 
 	// Clear existing leaderboard
-	leaderboard.innerHTML = "";
+	easyLeaderboard.innerHTML = "";
+	mediumLeaderboard.innerHTML = "";
+	hardLeaderboard.innerHTML = "";
 
-	//add entries to leaderboard
+	//add entries to specific leaderboard depending on difficulty
 	for (let i = 0; i < usernames.length; i++) {
-		const listItem = document.createElement("li");
-		const markElement = document.createElement("mark");
-		markElement.textContent = usernames[i].username; // Update to get username from the data
+		var user = usernames[i];
+		var listItem = document.createElement("li");
+		var markElement = document.createElement("mark");
+		markElement.textContent = usernames[i].username;
 		listItem.appendChild(markElement);
-		const smallElement = document.createElement("small");
-		smallElement.textContent = usernames[i].moves; // Update to get moves/score from the data
+		var smallElement = document.createElement("small");
+		smallElement.textContent = usernames[i].moves;
 		listItem.appendChild(smallElement);
-		leaderboard.appendChild(listItem);
+		console.log(user.difficulty);
+
+		if (user.difficulty == 10) {
+			easyLeaderboard.appendChild(listItem);
+			console.log("added to easy");
+		}
+		if (user.difficulty == 15) {
+			mediumLeaderboard.appendChild(listItem);
+			console.log("added to medium");
+		}
+		if (user.difficulty == 25) {
+			hardLeaderboard.appendChild(listItem);
+		}
 	}
 }
+
+const easyButton = document.getElementById("easy-button");
+const medButton = document.getElementById("medium-button");
+const hardButton = document.getElementById("hard-button");
+const scoreTitle = document.getElementById("score-title");
+
+easyButton.addEventListener("click", function () {
+	document.getElementById("medium-list").style.display = "none";
+	document.getElementById("score-list").style.display = "";
+	document.getElementById("hard-list").style.display = "none";
+	scoreTitle.textContent = "Easy Scores";
+	document.getElementById("easy-button").style.background =
+		"rgba(255, 255, 255, 0.2)";
+	document.getElementById("medium-button").style.background = "";
+	document.getElementById("hard-button").style.background = "";
+});
+
+medButton.addEventListener("click", function () {
+	document.getElementById("medium-list").style.display = "";
+	document.getElementById("score-list").style.display = "none";
+	document.getElementById("hard-list").style.display = "none";
+	scoreTitle.textContent = "Medium Scores";
+	document.getElementById("medium-button").style.background =
+		"rgba(255, 255, 255, 0.2)";
+	document.getElementById("easy-button").style.background = "";
+	document.getElementById("hard-button").style.background = "";
+});
+
+hardButton.addEventListener("click", function () {
+	document.getElementById("medium-list").style.display = "none";
+	document.getElementById("score-list").style.display = "none";
+	document.getElementById("hard-list").style.display = "";
+	scoreTitle.textContent = "Hard Scores";
+	document.getElementById("hard-button").style.background =
+		"rgba(255, 255, 255, 0.2)";
+	document.getElementById("medium-button").style.background = "";
+	document.getElementById("easy-button").style.background = "";
+});
 
 //call function when scores.html page is loaded
 window.onload = function () {
 	// Check if on the scores.html page
 	if (window.location.pathname.includes("scores.html")) {
-		//create leaderboard
+		//create leaderboard & default display easy scores
+		easyButton.click();
 		createLeaderboard();
 	}
 };
